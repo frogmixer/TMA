7053dadedbd8b6da1e9286b9afad11a0172831e0
Your Name <you@example.com>
2025-06-13 16:21:52 +0800

    update layout

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,6 +1,4 @@
-/* eslint-disable */
-
-import { HiX } from 'react-icons/hi';
+import { IoIosCloseCircleOutline  } from 'react-icons/io';
 import Links from './components/Links';
 
 import SidebarCard from 'components/sidebar/components/SidebarCard';
@@ -10,7 +8,7 @@ function SidebarHorizon(props: { routes: IRoute[]; [x: string]: any }) {
   const { routes, open, setOpen } = props;
   return (
     <div
-      className={`sm:none duration-175 linear fixed !z-50 flex min-h-full flex-col bg-white pb-10 shadow-2xl shadow-white/5 transition-all dark:!bg-navy-800 dark:text-white md:!z-50 lg:!z-50 xl:!z-0 ${
+      className={`sm:none duration-175  rounded-[150px]  linear fixed !z-50 flex min-h-full flex-col bg-white pb-10 shadow-2xl shadow-white/5 transition-all dark:!bg-navy-800 dark:text-white md:!z-50 lg:!z-50 xl:!z-0 ${
         open ? 'translate-x-0' : '-translate-x-96 xl:translate-x-0'
       }`}
     >
@@ -18,7 +16,7 @@ function SidebarHorizon(props: { routes: IRoute[]; [x: string]: any }) {
         className="absolute right-4 top-4 block cursor-pointer xl:hidden"
         onClick={() => setOpen(false)}
       >
-        <HiX />
+        <IoIosCloseCircleOutline size={36} />
       </span>
 
       <div className={`mx-[56px] mt-[50px] flex items-center`}>
